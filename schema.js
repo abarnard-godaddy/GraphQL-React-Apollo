adeb99c5cc110bdf65988029c42409b8dfde6c83
Aaron P Barnard <abarnard@lmtc-abarn.jomax.paholdings.com>
2019-05-17 16:07:28 -0700

    add launch and rocket gql type

--- schema.js
+++ schema.js
@@ -15,7 +15,7 @@ const LaunchType = new GraphQLObjectType ({
 });
 
 //Rocket Type
-const LaunchType = new GraphQLObjectType ({
+const RocketType = new GraphQLObjectType ({
     name: 'Rocket',
     fields: () => ({
         rocket_id: { type: GraphQLString },
@@ -31,7 +31,38 @@ const RootQuery = new GraphQLObjectType({
         launches: {
             type: new GraphQLList(LaunchType),
             resolve(parent, args) {
-                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                return axios
+                .get(`https://api.spacexdata.com/v3/launches`)
+                .then(res => res.data);
+            }
+        },
+        launch: {
+            type: LaunchType,
+            args: {
+                flight_number: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios
+                .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+                .then(res => res.data);
+            }
+        },
+        rockets: {
+            type: new GraphQLList(RocketType),
+            resolve(parent, args) {
+                return axios
+                .get(`https://api.spacexdata.com/v3/rockets`)
+                .then(res => res.data);
+            }
+        },
+        rocket: {
+            type: Rocketype,
+            args: {
+                id: { type: GraphQLInt }
+            },
+            resolve(parent, args) {
+                return axios
+                .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
                 .then(res => res.data);
             }
         }
